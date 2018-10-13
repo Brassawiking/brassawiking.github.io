1c8d179204e924bcf0bbf17ef21055bed9432050
brassawiking <brassawiking@gmail.com>
2018-10-13 22:30:07 +0200

    Updating document title

--- index.js
+++ index.js
@@ -8,17 +8,27 @@ const COLORS = [
   '#81ff76',
   '#7a76ff'
 ]
+const TITLES = [
+  'Brassawiking',
+  'Brassawiking | Art',
+  'Brassawiking | Ludum Dare',
+  'Brassawiking | Projects',
+  'Brassawiking | About'
+]
+
 
 let app = init(
   document.documentElement, 
   {
+    documentTitle: TITLES[0],
     bg: COLORS[0],
     pageOffset: '',
     pageIndex: 0,
 
     step (value) {
       this.pageIndex = ((this.pageIndex + value % PAGES) + PAGES) % PAGES
-      this.pageOffset = `translateX(${-this.pageIndex*100}vw)`
+      this.pageOffset = `translateX(${-this.pageIndex * 100}vw)`
+      this.documentTitle = TITLES[this.pageIndex]
       this.bg = COLORS[this.pageIndex]
     }
   }
